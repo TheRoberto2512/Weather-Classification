668b2cb73aebd49747b6ee8934d1ebb41539e357
Marco Cipollina <marco.cipo8@gmail.com>
2024-07-12 17:35:40 +0200

    Inserimento tuning KNN
    Aggiunto gridsearch per customKNN e modificata la classe per la compatibilità con sklearn

--- customKNN.py
+++ customKNN.py
@@ -1,15 +1,39 @@
 from Preprocessing import load_dataset
 import numpy as np
+from sklearn.base import BaseEstimator, ClassifierMixin
+
 
 from sklearn.metrics.pairwise import euclidean_distances,manhattan_distances
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_validate
 from sklearn.metrics import accuracy_score
 
 
 
-def customKNNClassifier(X_train, y_train, X_test, y_test, k=3):
+def tuningCustomKnn(X_train, y_train, X_test, y_test):
+    # Definiamo il range di valori k e la misura di distanza da adottare
+    valori_k = [k for k in range(1, 25)]
+    misure_distanze = ["Euclidea", "Manhattan"]
+
+    # Inizializzamo array per memorizzare le varie accuratezze durante il tuning
+    acc_val = np.empty((len(valori_k), len(misure_distanze)))
+
+    for i, k in enumerate(valori_k):
+        for j, misura_distanza in enumerate(misure_distanze):
+                custom_knn = CustomKNN(k, misura_distanza)
+                cv_accuratezze = cross_validate(estimator=custom_knn, X=X_train, y=y_train, cv=3, n_jobs=6)
+                # Memorizza le accuratezze medie nei rispettivi array
+                acc_val[i, j] = cv_accuratezze['test_score'].mean()
+                print("\nAggiunta la media delle accuratezze per k uguale a %d e misura di distanza %s:\t%.5f"%(k, misura_distanza, acc_val[i,j]))
+    i, j = np.unravel_index(np.argmax(acc_val, axis=None), acc_val.shape)
+    k_migliore = valori_k[i]
+    misura_distanza_migliore = misure_distanze[j]
+    print("La miglior accuratezza è %.5f ed è stata ottenuta con la distanza di %s e con k uguale a %d\n"% (acc_val[i,j], misura_distanza_migliore, k_migliore))
+
+
+
+def customKNNClassifier(X_train, y_train, X_test, y_test, k=3, misura_distanza = "Euclidea"):
     # Inizializo il modello
-    custom_knn = CustomKNN(k)
+    custom_knn = CustomKNN(k, misura_distanza)
 
     # Addestro il modello
     custom_knn.fit(X_train, y_train)
@@ -22,10 +46,16 @@ def customKNNClassifier(X_train, y_train, X_test, y_test, k=3):
   
 
 #classe custom del knn che permette di effettuare le previsioni pesando il contributo di ogni vicino in base alla sua vicinanza al nuovo record
-class CustomKNN:
+class CustomKNN(BaseEstimator):
+    """
+    BaseEstimator e ClassifierMixin utilizzati come superclassi forniscono
+    una struttura e un'implementazione che rendono il modello 
+    compatibile col cross_validate di scikit-learn.
+    """
     #nizializza il valore di k
-    def __init__(self, k=3):
+    def __init__(self, k=3, misura_distanza = "Euclidea"):
         self.k = k
+        self.misura_distanza = misura_distanza
         self.fittato=False
 
     #metodo che addestra il modello memorizzando il dataset
@@ -49,7 +79,12 @@ class CustomKNN:
                 # Reshape della istanza per essere un 2D array
                 istanza_reshaped = X[istanza].reshape(1, -1)
                 #calcolo la distanza del record corrente dai record del training set
-                distanze = euclidean_distances(self.mX, istanza_reshaped).flatten()
+                if self.misura_distanza == "Euclidea":
+                    distanze = euclidean_distances(self.mX, istanza_reshaped).flatten()
+                elif self.misura_distanza == "Manhattan":
+                    distanze = manhattan_distances(self.mX, istanza_reshaped).flatten()
+                else:
+                    return print("\nLa misura di distanza richiesta non è supportata\n")
                 #prendo gli indici delle k istanze più vicine
                 indici=np.argsort(distanze)[:self.k]
                 #prendo le distanze delle k istanze più vicine 
@@ -75,8 +110,7 @@ class CustomKNN:
         if self.fittato:
            #DIPENDE DA COME VOGLIAMO VALUTARE IL MODELLO
             pred_y=self.predict(X_test)
-            accuracy = accuracy_score(y_test, pred_y)
-            print(accuracy)
+            return accuracy_score(y_test, pred_y)
         else:
             print("E' prima necessario addestrare il modello")
 
@@ -86,5 +120,5 @@ if __name__ == '__main__':
     X, y = load_dataset()
     train_X, test_X, train_y, test_y = train_test_split(X, y, random_state=0, test_size=0.25)
 
-    customKNNClassifier(train_X, train_y, test_X, test_y)
+    tuningCustomKnn(train_X, train_y, test_X, test_y)
     print("\n\nFine codice\n\n")
